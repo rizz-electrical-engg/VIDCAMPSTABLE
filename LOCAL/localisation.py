f4d5227377e05773876cb0fde842f58964ded449
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-09-24 21:46:40 +0530

    Add files via upload

---
+++ LOCAL/localisation.py
@@ -0,0 +1,45 @@
+#Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
+#Respect my work by not making any changes here.
+
+START_TEXT = "Send me any file to begin."
+
+FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
+
+CHANNEL_LINK = "https://t.me/aboutRizzx"
+
+SUPPORT_LINK = "https://t.me/ContactM_ebot"
+
+info_text = "This bot is developed by ⏤͟͞𝙍𝙞𝙯𝙯🍀\n\nWritten in python library TELETHON.\n\nBot by : @aboutRizzx\nSupport : @ContactM_ebot\n\nV1.4"   
+
+help_text = """**v1.4**
+
+•`Encode` - encode your video into different lib format or resolution
+
+•`HEVC compress` - negligible loss compression
+
+•`FAST compress` - Very fast and Efficient compression
+
+•`Convert` - change formats or extract audio of any video
+
+•`Rename` - rename any file, extension not required
+
+•`SSHOTS` - generate 10 screenshots of your video
+
+•`Trim` - cut your videos"""
+
+source_text = "**Deploy your own bot**"
+
+DEV = "https://t.me/aboutRizzx"
+
+spam_notice = "Spamming the bot or encoding adult videos will lead you to a ban."
+
+JPG = "LOCAL/video_convertor.jpg"
+
+JPG0 = 'https://envs.sh/PvC.jpg'
+
+JPG2 = "LOCAL/Pasted image.jpg"
+
+JPG3 = "LOCAL/bhartiyee.jpg"
+
+JPG4 = "LOCAL/tg@aboutRizzx.jpg"
+
